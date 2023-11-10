54ac5c4fa6f208f8bab94646f05d3c5f8631849c
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2023-11-10 17:33:28 +0100

    add: 12 fatiche

--- le-12-fatiche-di-javascript/app.js
+++ le-12-fatiche-di-javascript/app.js
@@ -1,24 +1,24 @@
 const input = document.querySelector('input');
-const array = null;
+const array = [];//1 c'era null e non []
 
 input.addEventListener('keypress', function(event) {
 
     if (event.code != 'Enter')    return;
-    if (input.value.length == '') return;
+    if (input.value.length == 0) return; //2 c'era stinga vuota al posto di 0
 
-    array.add(input.value);
+    array.push(input.value);//3c'era add
 
     const li = document.createElement('li');
     li.classList.add('list-group-item');
-    li.text = input.value;
-    document.querySelector('ul').push(li);
+    li.innerHTML = input.value; //mancava .innerHTML
+    document.querySelector('ul').append(li);//4c'era push non append
 
-    let counter = '';
+    let counter = 0; //5deve essere numero non stringa
     let item    = array[0];
-    const max   = 1;
+    let max   = 1;//9 era const
     const elems = [];
 
-    for (let i = 0; i < array; i++) {
+    for (let i = 0; i < array.length; i++) {//6 mancava .length
         let val = array[i];
 
         if (!elems[val]) {
@@ -27,19 +27,20 @@ input.addEventListener('keypress', function(event) {
             elems[val]++;
         }
 
-        for (let j = i; j < array.length; i++) {
+        for (let j = i; j < array.length; j++) { //7 c'era i++ al posto di j++
             if (array[i] == array[j]) {
                 counter++;
-                if (max < counter)
+                if (max < counter){ //8 mancano le graffe
                 max  = counter;
                 item = array[i];
+                }
             }
         }
 
         counter = 0;
     }
 
-    const alert = document.getElementsByClassName('alert');
+    const alert = document.querySelector('.alert');//non c'era querrySelector
 
     alert.classList.remove('d-none');
     alert.classList.add('d-flex');
@@ -47,5 +48,5 @@ input.addEventListener('keypress', function(event) {
     alert.querySelector('span:first-child').innerText = item;
     alert.querySelector('span:last-child').innerText = max;
 
-    console.log('${item} trovato ${max} volte');
+    console.log(`${item} trovato ${max} volte`);//12 `` non ''
 });
